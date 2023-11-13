764e528d5882bd3eab33a7d45908b070b654d109
bugagus <agustin.tn.02@gmail.com>
2023-11-13 13:56:44 +0100

    Cronometro
    Se ha implementado un cronometro que va cambiando la GUI de manera correcta. El cronometro tiene un metodo que permite reiniciarlo.
    
    IMPORTANTE: El prefab es necesario meterlo en un canvas, que nadie se alarme si al soltarlo tal cual en el mundo no lo ve.

---
+++ Project Aura/Assets/ChronometerController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChronometerController : MonoBehaviour
+{
+
+    private float _timeElapsed;
+    [SerializeField] private TextMeshProUGUI textSeconds;
+    [SerializeField] private TextMeshProUGUI textMinutes;
+
+    private void Start()
+    {
+        _timeElapsed = 0;
+    }
+
+    void Update()
+    {
+        _timeElapsed += Time.deltaTime;
+        PrintTimer();
+    }
+
+    public void ResetTimer()
+    {
+        _timeElapsed = 0;
+    }
+
+    public void PrintTimer()
+    {
+        int _seconds = (int) _timeElapsed;
+        int _minutes = 0;
+        while (_seconds > 60)
+        {
+            _seconds -= 60;
+            _minutes += 1;
+        }
+        Debug.Log(_seconds);
+        Debug.Log(_minutes);
+        if (_minutes < 10)
+        {
+            textMinutes.text = ("0") + _minutes;
+        }
+        else
+        {
+            textMinutes.text = _minutes.ToString();
+        }
+
+        if (_seconds < 10)
+        {
+            textSeconds.text = ("0") + _seconds;
+        }
+        else
+        {
+            textSeconds.text = _seconds.ToString();
+        }
+    }
+}
