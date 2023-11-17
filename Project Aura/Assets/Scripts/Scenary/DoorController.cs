07588be91949c9910286b1d47e2a0e03737105a8
Ivangutierreza <ivanjoerugby15@gmail.com>
2023-11-17 14:04:24 +0100

    Selector de niveles final

---
+++ Project Aura/Assets/Scripts/Scenary/DoorController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorController : MonoBehaviour
+{
+    public int scene;
+    public GameObject interactionText;
+
+    private bool touch;
+    private void Update()
+    {
+        touch = false;
+    }
+    public void changeScene()
+    {
+        Debug.Log("Cambiando");
+        SceneManager.LoadScene(scene);
+        
+    }
+
+    public void apearText()
+    {
+        
+        if (touch == true)
+        Debug.Log("Texto");
+        interactionText.SetActive(true);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Tocando");
+            touch = true;
+        }
+          
+    }
+}
